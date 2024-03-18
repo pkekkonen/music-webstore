8a04e3cf6c9a78258b646551eacb4fa369c17a59
pkekkonen <paulina.kekkonen@hotmail.com>
2024-03-18 13:56:03 +0100

    Small refactoring

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,9 @@
 import BrowseBody from "./BrowseBody";
 
 function Body() {
-    const handleSortSelection = (event) => {
-        console.log(event.target.value)
-    }
 
     return (
     <>
-      <select id="sort-by" onChange={handleSortSelection}>
-        <option value="most-popular">Most popular</option>
-        <option value="release-date">Latest released</option>
-        <option value="title-alphabetically">Title, alphabetically</option>
-        <option value="title-reverese-alphabetically">
-          Title, reversed alphabetically
-        </option>
-        <option value="price-low-to-high">Price, low to high</option>
-        <option value="price-high-to-low">Price, high to low</option>
-      </select>
       <BrowseBody></BrowseBody>
     </>
   );
--- src/components/BrowseBody.jsx
+++ src/components/BrowseBody.jsx
@@ -3,5 +3,23 @@ import { ProductContext } from "./Store";
 import ProductList from "./ProductList";
 
 export default function BrowseBody() {
-  return <ProductList />;
+  const handleSortSelection = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <>
+      <select id="sort-by" onChange={handleSortSelection}>
+        <option value="most-popular">Most popular</option>
+        <option value="release-date">Latest released</option>
+        <option value="title-alphabetically">Title, alphabetically</option>
+        <option value="title-reverese-alphabetically">
+          Title, reversed alphabetically
+        </option>
+        <option value="price-low-to-high">Price, low to high</option>
+        <option value="price-high-to-low">Price, high to low</option>
+      </select>
+      <ProductList />
+    </>
+  );
 }
